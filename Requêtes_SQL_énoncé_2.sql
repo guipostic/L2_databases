7fc77e5b7ba11e32368b1ffeacfc89197d7e31ea
Guillaume Postic <guillaume.yann.postic@gmail.com>
2022-10-25 23:16:38 +0200

    Update Requêtes_SQL_énoncé_2.sql

--- Requêtes_SQL_énoncé_2.sql
+++ Requêtes_SQL_énoncé_2.sql
@@ -5,19 +5,19 @@ SELECT * FROM Film;
 SELECT Titre FROM Film;
 
 /*3*/
-SELECT Nom FROM Artiste WHERE Annee-Naissance > 1995;
+SELECT Nom FROM Artiste WHERE Annee_Naissance > 1995;
 
 /*4*/
-SELECT Nom-c FROM Cinema WHERE Arrondissement = 13;
+SELECT Nom_c FROM Cinema WHERE Arrondissement = 13;
 
 /*5*/
 SELECT Libelle FROM Role WHERE Acteur = "Brad Pitt";
 
 /*6*/
-SELECT Libelle FROM Role WHERE Acteur IN (SELECT Nom FROM Artiste WHERE Annee-Naissance < 1970);
+SELECT Libelle FROM Role WHERE Acteur IN (SELECT Nom FROM Artiste WHERE Annee_Naissance < 1970);
 
-SELECT Libelle FROM Role r, Artiste a WHERE (r.Acteur = a.Nom AND a.Annee-Naissance < 1970);
-SELECT Libelle FROM Role, Artiste WHERE (Acteur = Nom AND Annee-Naissance < 1970);
+SELECT Libelle FROM Role r, Artiste a WHERE (r.Acteur = a.Nom AND a.Annee_Naissance < 1970);
+SELECT Libelle FROM Role, Artiste WHERE (Acteur = Nom AND Annee_Naissance < 1970);
 
 /*7*/
 SELECT Nom FROM Artiste WHERE Nom LIKE "B%";
@@ -39,81 +39,82 @@ SELECT Titre FROM Film WHERE Annee >= 1980 AND Annee <= 1990;
 SELECT Titre FROM Film WHERE Realisateur = "Woody Allen";
 
 /*13*/
+/* renvoie films où acteur ET réal */
 SELECT Titre FROM Film f
 		JOIN Role r
-		ON f.IDFilm = r.IDFilm
-		WHERE r.Acteur = "Clint Eastwood" OR f.Realisateur = "Clint Eastwood";
+		ON f.ID_Film = r.ID_Film
+		WHERE r.Acteur = "Clint Eastwood" OR f.Realisateur = "Clint Eastwood"; 
 
-SELECT Titre FROM Film f, Role r WHERE f.IDFilm = r.IDFilm AND (Acteur = "Clint Eastwood" OR Realisateur = "Clint Eastwood");
+SELECT Titre FROM Film f, Role r WHERE f.ID_Film = r.ID_Film AND (Acteur = "Clint Eastwood" OR Realisateur = "Clint Eastwood");
 
 /*14*/
 SELECT * FROM Film ORDER BY Titre;
 
 /*15*/
 SELECT * FROM Seance s
-	JOIN Cinema c ON s.Nom-c = c.Nom-c
+	JOIN Cinema c ON s.Nom_c = c.Nom_c
 	WHERE Heure_fin < 12 AND Arrondissement = 13;
 
 /*16*/
-SELECT r.Acteur FROM Film f JOIN Role r ON f.IDFilm = r.IDFilm AND f.Realisateur = r.Acteur;
-SELECT f.Realisateur FROM Film f, Role r WHERE f.IDFilm = r.IDFilm AND f.Realisateur = r.Acteur;
+SELECT r.Acteur FROM Film f JOIN Role r ON f.ID_Film = r.ID_Film AND f.Realisateur = r.Acteur;
+SELECT f.Realisateur FROM Film f, Role r WHERE f.ID_Film = r.ID_Film AND f.Realisateur = r.Acteur;
 
 /*17*/
 SELECT Nom FROM Artiste WHERE Nom NOT IN (SELECT Realisateur FROM Film);
-SELECT Nom FROM Artiste MINUS (SELECT Realisateur FROM Film);
+SELECT Nom FROM Artiste MINUS (SELECT Realisateur FROM Film); /* MINUS ne fonctionne pas avec tous les SGBD */
 
 /*18*/
 SELECT Realisateur
 FROM Film
-WHERE IDFilm NOT IN (
-	SELECT IDFilm
+WHERE ID_Film NOT IN (
+	SELECT ID_Film
 	FROM Seance se
 	JOIN Salle sa
-	ON se.No_salle = sa.No_Salle AND se.Nom-c = sa.Nom-c
+	ON se.No_salle = sa.No_salle AND se.Nom_c = sa.Nom_c
 	WHERE sa.Capacite > 200
 );
 
 /*19*/
-SELECT COUNT(Nom-c) FROM Cinema WHERE Arrondissement = 13;
+SELECT COUNT(Nom_c) FROM Cinema WHERE Arrondissement = 13;
 
 
 /*20*/
-SELECT AVG(Capacite) FROM Salle WHERE Climatise IS NOT NULL;
+SELECT AVG(Capacite) FROM Salle WHERE Climatise = 1;
 
 /*21*/
-SELECT COUNT(*) FROM Salle WHERE Nom-c = "Escurial";
+SELECT COUNT(*) FROM Salle WHERE Nom_c = "Escurial";
 
 
 
 /*22*/
 SELECT MAX(nombre_salles), AVG(nombre_salles)
 FROM (
-	SELECT Nom-c, SUM(compte) AS nombre_salles
+	SELECT Nom_c, SUM(compte) AS nombre_salles
 	FROM (
-		SELECT Nom-c, 1 AS compte
+		SELECT Nom_c, 1 AS compte
 		FROM Salle
 	) AS table1
-	GROUP BY Nom-c
+	GROUP BY Nom_c
 ) AS table2;
 
 
 /*23*/
-SELECT Nom-c, SUM(compte) AS nombre_salles
+SELECT Nom_c, SUM(compte) AS nombre_salles
 FROM (
-	SELECT Nom-c, 1 AS compte
+	SELECT Nom_c, 1 AS compte
 	FROM Salle
-)
-GROUP BY Nom-c
+) table0
+GROUP BY Nom_c
 WHERE nombre_salles
 IN (
 	SELECT MAX(nombre_salles)
 	FROM (
-		SELECT Nom-c, SUM(compte) AS nombre_salles
+		SELECT Nom_c, SUM(compte) AS nombre_salles
 		FROM (
-			SELECT Nom-c, 1 AS compte
+			SELECT Nom_c, 1 AS compte
 			FROM Salle
 		) AS table1
-		GROUP BY Nom-c
+		GROUP BY Nom_c
 	) AS table2
 );
 
